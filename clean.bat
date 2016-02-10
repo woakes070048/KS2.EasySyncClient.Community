8e69148ac2d80ecbefc5649d8b157a7de21e7648
KaliConseil <contact@kaliconseil.fr>
2016-02-10 12:33:55 +0100

    R1

---
+++ clean.bat
@@ -0,0 +1,28 @@
+cd ./DotCMIS
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
+cd KS2.EasySync.CMISOnly
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
+cd KS2.EasySync.Core
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
+cd KS2.EasySync.Interface
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
+cd KS2.Easysync.Mac
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
+cd KS2.EasySync.Windows
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
+cd KS2.EasySyncClient.Community
+rmdir /s /q obj
+rmdir /s /q bin
+cd ..
